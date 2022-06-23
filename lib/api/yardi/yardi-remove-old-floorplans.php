67435c8f76704d8aa66f8fcfaed364968696ec34
David Faltermier <david@codegeek.net>
2022-06-23 17:38:30 -0600

    Resolve debug.log error messages: (1) using null as array index, (2) accessing null as array object.

--- lib/api/yardi/yardi-remove-old-floorplans.php
+++ lib/api/yardi/yardi-remove-old-floorplans.php
@@ -2,10 +2,10 @@
 
 add_action( 'rentfetch_do_remove_old_data', 'rentfetch_do_remove_floorplans_from_orphan_yardi_properties' );
 function rentfetch_do_remove_floorplans_from_orphan_yardi_properties() {
-            
+
     $sync_term = get_field( 'sync_term', 'option' );
     $data_sync = get_field( 'data_sync', 'option' );
-    
+
     // if syncing is paused or data dync is off, then then bail, as we won't be restarting anything
     if ( $sync_term == 'paused' || $data_sync == 'delete' || $data_sync == 'nosync' ) {
         as_unschedule_action( 'rentfetch_do_remove_floorplans_from_orphan_yardi_properties_specific', array(), 'yardi' );
@@ -14,17 +14,17 @@ function rentfetch_do_remove_floorplans_from_orphan_yardi_properties() {
         as_unschedule_all_actions( 'rentfetch_do_remove_orphan_yardi_properties', array(), 'yardi' );
         return;
     }
-    
+
     if ( as_next_scheduled_action( 'rentfetch_do_remove_floorplans_from_orphan_yardi_properties_specific', array(), 'yardi' ) == false ) {
         rentfetch_verbose_log( "Scheduling regular task to remove floorplans from properties that are no longer set to sync." );
         as_schedule_recurring_action( time(), rentfetch_get_sync_term_in_seconds(), 'rentfetch_do_remove_floorplans_from_orphan_yardi_properties_specific', array(), 'yardi' );
     }
-    
+
     if ( as_next_scheduled_action( 'rentfetch_do_remove_orphan_yardi_properties', array(), 'yardi' ) == false ) {
         rentfetch_verbose_log( "Scheduling regular task to remove properties that are no longer set to sync." );
         as_schedule_recurring_action( time(), rentfetch_get_sync_term_in_seconds(), 'rentfetch_do_remove_orphan_yardi_properties', array(), 'yardi' );
     }
- 
+
 }
 
 function rentfetch_get_meta_values( $key = '', $type = 'post', $status = 'publish' ) {
@@ -37,11 +37,11 @@ function rentfetch_get_meta_values( $key = '', $type = 'post', $status = 'publis
     $r = $wpdb->get_col( $wpdb->prepare( "
         SELECT pm.meta_value FROM {$wpdb->postmeta} pm
         LEFT JOIN {$wpdb->posts} p ON p.ID = pm.post_id
-        WHERE pm.meta_key = %s 
-        AND p.post_status = %s 
+        WHERE pm.meta_key = %s
+        AND p.post_status = %s
         AND p.post_type = %s
     ", $key, $status, $type ) );
-    
+
     return $r;
 }
 
@@ -50,41 +50,41 @@ function rentfetch_get_meta_values( $key = '', $type = 'post', $status = 'publis
 
 add_action( 'rentfetch_do_remove_orphan_yardi_properties', 'rentfetch_remove_orphan_yardi_properties' );
 function rentfetch_remove_orphan_yardi_properties() {
-    
+
     rentfetch_verbose_log( "Running script to delete orphan properties from Yardi." );
-    
+
     $property_ids_attached_to_properties = rentfetch_get_meta_values( 'voyager_property_code', 'properties' );
     $property_ids_attached_to_properties = array_unique( $property_ids_attached_to_properties );
     $property_ids_attached_to_properties = array_map('strtolower', $property_ids_attached_to_properties); // lowercase everything, as case mismatches can give us bad results
-    
+
     // echo 'In the database: ' . count( $property_ids_attached_to_properties ) . '<br/>';
     // var_dump( $property_ids_attached_to_properties );
 
     // get the property ids from the setting
     $yardi_integration_creds = get_field( 'yardi_integration_creds', 'option' );
-    $yardi_api_key = $yardi_integration_creds['yardi_api_key'];
-    $properties_in_setting = $yardi_integration_creds['yardi_property_code'];
+    $yardi_api_key = $yardi_integration_creds['yardi_api_key'] ?? '';
+    $properties_in_setting = $yardi_integration_creds['yardi_property_code'] ?? '';
     $properties_in_setting = preg_replace('/\s+/', '', $properties_in_setting);
     $properties_in_setting = explode( ',', $properties_in_setting );
     $properties_in_setting = array_unique( $properties_in_setting );
     $properties_in_setting = array_map('strtolower', $properties_in_setting); // lowercase everything, as case mismatches can give us bad results
-    
+
     // echo 'In setting: ' . count( $properties_in_setting ) . '<br/>';
     // var_dump( $properties_in_setting );
-    
+
     // get the ones that are in the database, but that aren't in the setting
     $properties = array_diff( $property_ids_attached_to_properties, $properties_in_setting );
-    
+
     // var_dump( $properties );
     // echo 'Diff: ' . count( $properties );
     // var_dump( $properties );
-    
+
     // bail if there's nothing to delete
     if ( empty( $properties )) {
         rentfetch_verbose_log( "No orphan properties found." );
         return;
     }
-            
+
     $args = array(
         'post_type' => 'properties',
         'posts_per_page' => -1,
@@ -103,15 +103,15 @@ function rentfetch_remove_orphan_yardi_properties() {
             ),
         ),
     );
-    
+
     $properties_to_delete = new WP_Query($args);
     if ( $properties_to_delete->have_posts() ) {
         while ( $properties_to_delete->have_posts() ) {
             $properties_to_delete->the_post();
-            
+
             $property_id = get_the_ID();
             $voyager_property_code = get_post_meta( $property_id, 'voyager_property_code', true );
-            
+
             rentfetch_log( "Deleting property $voyager_property_code." );
             wp_delete_post( $property_id, true );
         }
@@ -123,37 +123,37 @@ function rentfetch_remove_orphan_yardi_properties() {
 
 add_action( 'rentfetch_do_remove_floorplans_from_orphan_yardi_properties_specific', 'rentfetch_remove_floorplans_from_orphan_yardi_properties_specific' );
 function rentfetch_remove_floorplans_from_orphan_yardi_properties_specific() {
-    
+
     // get the property ids which exist in our floorplans CPT 'property_id' meta field
     $property_ids_attached_to_floorplans = rentfetch_get_meta_values( 'voyager_property_code', 'floorplans' );
     $property_ids_attached_to_floorplans = array_unique( $property_ids_attached_to_floorplans );
-    
+
     // get the property ids from the setting
     $yardi_integration_creds = get_field( 'yardi_integration_creds', 'option' );
-    $yardi_api_key = $yardi_integration_creds['yardi_api_key'];
-    $properties_in_setting = $yardi_integration_creds['yardi_property_code'];
+    $yardi_api_key = $yardi_integration_creds['yardi_api_key'] ?? '';
+    $properties_in_setting = $yardi_integration_creds['yardi_property_code'] ?? '';
     $properties_in_setting = preg_replace('/\s+/', '', $properties_in_setting);
     $properties_in_setting = explode( ',', $properties_in_setting );
-    
+
     // get the ones that are in floorplans, but that aren't in the setting
     $properties = array_diff( $property_ids_attached_to_floorplans, $properties_in_setting );
-    
+
     if ( $properties == null )
         return;
-    
+
     // for each property that's in the DB but not in our list, do a query for floorplans that correspond, then delete those
     foreach( $properties as $property ) {
-        
+
         // remove upcoming actions for pulling floorplans from the API
         rentfetch_verbose_log( "Property $property found in published floorplans, but not found in setting. Removing upcoming api actions." );
         as_unschedule_action( 'rentfetch_do_get_yardi_floorplans_from_api_for_property', array( $property, $yardi_api_key ), 'yardi' );
         as_unschedule_all_actions( 'rentfetch_do_get_yardi_floorplans_from_api_for_property', array( $property, $yardi_api_key ), 'yardi' );
-        
+
         // remove upcoming actions for syncing floorplans
         rentfetch_verbose_log( "Property $property found in published floorplans, but not found in setting. Removing upcoming CPT update actions." );
         as_unschedule_action( 'rentfetch_do_fetch_yardi_floorplans', array( $property ), 'yardi' );
         as_unschedule_all_actions( 'rentfetch_do_fetch_yardi_floorplans', array( $property ), 'yardi' );
-        
+
         $args = array(
             'post_type' => 'floorplans',
             'posts_per_page' => -1,
@@ -172,15 +172,15 @@ function rentfetch_remove_floorplans_from_orphan_yardi_properties_specific() {
                 ),
             ),
         );
-        
+
         $floorplan_query = new WP_Query($args);
         $floorplanstodelete = $floorplan_query->posts;
-        
+
         foreach ($floorplanstodelete as $floorplantodelete) {
             rentfetch_verbose_log( "Deleting floorplan $floorplantodelete->ID." );
             wp_delete_post( $floorplantodelete->ID, true );
         }
-                
+
     }
-    
+
 }
--- rent-fetch.php
+++ rent-fetch.php
@@ -45,13 +45,13 @@ define( 'RENTFETCH_ACF_PATH', plugin_dir_path( __FILE__ ) . 'vendor/acf/' );
 define( 'RENTFETCH_ACF_URL', plugin_dir_url( __FILE__ ) . 'vendor/acf/' );
 
 if( !class_exists('ACF') ) {
-    
+
     // Include the ACF plugin.
     include_once( RENTFETCH_ACF_PATH . 'acf.php' );
-    
+
     // Customize the url setting to fix incorrect asset URLs.
     add_filter('acf/settings/url', 'rentfetch_acf_settings_url');
-    
+
 }
 
 function rentfetch_acf_settings_url( $url ) {
@@ -61,27 +61,27 @@ function rentfetch_acf_settings_url( $url ) {
 //! UNCOMMENT THIS FILTER TO SAVE ACF FIELDS TO PLUGIN
 // add_filter('acf/settings/save_json', 'rentfetch_acf_json_save_point');
 function rentfetch_acf_json_save_point( $path ) {
-    
+
     // update path
     $path = RENTFETCH_DIR . 'acf-json';
-    
+
     // return
     return $path;
-    
+
 }
 
 add_filter( 'acf/settings/load_json', 'rentfetch_acf_json_load_point' );
 function rentfetch_acf_json_load_point( $paths ) {
-    
+
     // remove original path (optional)
     unset($paths[0]);
-    
+
     // append path
     $paths[] = RENTFETCH_DIR . 'acf-json';
-    
+
     // return
     return $paths;
-    
+
 }
 
 ///////////////////////
@@ -92,26 +92,26 @@ use AC\ListScreenRepository\Storage\ListScreenRepositoryFactory;
 use AC\ListScreenRepository\Rules;
 use AC\ListScreenRepository\Rule;
 add_filter( 'acp/storage/repositories', function( array $repositories, ListScreenRepositoryFactory $factory ) {
-    
+
     //! Change $writable to true to allow changes to columns for the content types below
     $writable = false;
-    
+
     // 2. Add rules to target individual list tables.
     // Defaults to Rules::MATCH_ANY added here for clarity, other option is Rules::MATCH_ALL
     $rules = new Rules( Rules::MATCH_ANY );
     $rules->add_rule( new Rule\EqualType( 'floorplans' ) );
     $rules->add_rule( new Rule\EqualType( 'properties' ) );
     $rules->add_rule( new Rule\EqualType( 'neighborhoods' ) );
-    
+
     // 3. Register your repository to the stack
     $repositories['rent-fetch'] = $factory->create(
         RENTFETCH_DIR . '/acp-settings',
         $writable,
         $rules
     );
-    
+
     return $repositories;
-    
+
 }, 10, 2 );
 
 ///////////////////
@@ -187,7 +187,7 @@ require_once( 'block/floorplangrid/floorplangrid.php' );
 
 add_action( 'init', 'rentfetch_start_sync' );
 function rentfetch_start_sync() {
-    
+
     //* REMOVE ALL ACTIONS ADDED BY OLD APARTMENTSYNC, ADDED IN 3.0, CAN BE REMOVED IN A LATER RELEASE
     as_unschedule_action( 'apartmentsync_do_get_yardi_property_from_api' );
     as_unschedule_all_actions( 'apartmentsync_do_get_yardi_property_from_api' );
@@ -201,44 +201,44 @@ function rentfetch_start_sync() {
     as_unschedule_all_actions( 'do_get_yardi_floorplans_from_api_for_property' );
     as_unschedule_action( 'do_fetch_yardi_floorplans' );
     as_unschedule_all_actions( 'do_fetch_yardi_floorplans' );
-    
+
     $sync_term = get_field( 'sync_term', 'option' );
     $data_sync = get_field( 'data_sync', 'option' );
-            
-    if ( $sync_term == 'paused' || $data_sync == 'delete' || $data_sync == 'nosync' ) {       
-         
+
+    if ( $sync_term == 'paused' || $data_sync == 'delete' || $data_sync == 'nosync' ) {
+
         as_unschedule_action( 'rentfetch_do_get_yardi_property_from_api' );
         as_unschedule_all_actions( 'rentfetch_do_get_yardi_property_from_api' );
         as_unschedule_action( 'rentfetch_do_get_yardi_floorplans_from_api_for_property' );
         as_unschedule_all_actions( 'rentfetch_do_get_yardi_floorplans_from_api_for_property' );
         as_unschedule_action( 'rentfetch_do_fetch_yardi_floorplans' );
         as_unschedule_all_actions( 'rentfetch_do_fetch_yardi_floorplans' );
-        
+
     } else {
-        
+
         //* We're doing these async because we don't want them constantly triggering on each pageload. We'd still like to bundle together our syncing and our chron
-        if ( as_next_scheduled_action( 'rentfetch_do_sync_logic' ) === false  ) 
+        if ( as_next_scheduled_action( 'rentfetch_do_sync_logic' ) === false  )
             as_enqueue_async_action( 'rentfetch_do_sync_logic' );
-            
-        if ( as_next_scheduled_action( 'rentfetch_do_chron_activation' ) === false  ) 
+
+        if ( as_next_scheduled_action( 'rentfetch_do_chron_activation' ) === false  )
             as_enqueue_async_action( 'rentfetch_do_chron_activation' );
-            
-        if ( as_next_scheduled_action( 'rentfetch_do_remove_old_data' ) === false  ) 
+
+        if ( as_next_scheduled_action( 'rentfetch_do_remove_old_data' ) === false  )
             as_enqueue_async_action( 'rentfetch_do_remove_old_data' );
-            
+
     }
-        
+
     //* Delete everything if we're set to delete
     if ( $data_sync == 'delete' )
         do_action( 'rentfetch_do_delete' );
-            
+
     // do_action( 'rentfetch_do_sync_logic' );
     // do_action( 'rentfetch_do_chron_activation' );
-        
+
     // // Look and see whether there's another scheduled action waiting
-    // var_dump( as_next_scheduled_action( 'rentfetch_do_sync_logic' ) ); 
+    // var_dump( as_next_scheduled_action( 'rentfetch_do_sync_logic' ) );
     // var_dump( as_next_scheduled_action( 'rentfetch_do_chron_activation' ) );
-    
+
 }
 
 //////////////////////
@@ -248,31 +248,31 @@ function rentfetch_start_sync() {
 
 add_action( 'init', 'rentfetch_register_content_types' );
 function rentfetch_register_content_types() {
-            
-    //* figure out whether this is a single 
+
+    //* figure out whether this is a single
     $apartment_site_type = get_field( 'apartment_site_type', 'option' );
-        
+
     //* only register the properties and neighborhoods post types if this is a 'multiple' site
     if ( $apartment_site_type == 'multiple' ) {
-        
+
         // properties cpt
         add_action( 'init', 'rentfetch_register_properties_cpt', 20 );
-        
+
         // amenities property taxes
         add_action( 'init', 'rentfetch_register_amenities_taxonomy', 20 );
         add_action( 'init', 'rentfetch_register_propertytype_taxonomy', 20 );
-        
+
         // neighborhoods cpt
         add_action( 'init', 'rentfetch_register_neighborhoods_cpt', 20 );
-        
+
         // neighborhoods taxes
         add_action( 'init', 'rentfetch_register_areas_taxonomy', 20 );
-        
+
         // connect properties and neighborhoods
         require_once( 'lib/cpt-connections/properties-to-neighborhoods.php' );
-        
+
     }
-    
+
 }
 
 //////////////
@@ -281,58 +281,58 @@ function rentfetch_register_content_types() {
 
 add_action( 'wp_enqueue_scripts', 'rentfetch_enqueue_scripts_stylesheets' );
 function rentfetch_enqueue_scripts_stylesheets() {
-	
+
 	// Plugin styles
     wp_register_style( 'rentfetch-single-properties', RENTFETCH_PATH . 'css/single-properties.css', array(), RENTFETCH_VERSION, 'screen' );
-    
+
     // NoUISlider (for dropdown double range slider)
     wp_register_style( 'rentfetch-nouislider-style', RENTFETCH_PATH . 'vendor/nouislider/nouislider.min.css', array(), RENTFETCH_VERSION, 'screen' );
     wp_register_script( 'rentfetch-nouislider-script', RENTFETCH_PATH . 'vendor/nouislider/nouislider.min.js', array( 'jquery' ), RENTFETCH_VERSION, true );
     wp_register_script( 'rentfetch-nouislider-init-script', RENTFETCH_PATH . 'js/rentfetch-search-map-nouislider-init.js', array( 'jquery' ), RENTFETCH_VERSION, true );
-    
+
     // Flatpickr
     wp_register_style( 'rentfetch-flatpickr-style', 'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css', array(), RENTFETCH_VERSION, 'screen' );
     wp_register_script( 'rentfetch-flatpickr-script', 'https://cdn.jsdelivr.net/npm/flatpickr', array( 'jquery' ), RENTFETCH_VERSION, true );
     wp_register_script( 'rentfetch-flatpickr-script-init', RENTFETCH_PATH . 'js/rentfetch-search-map-flatpickr-init.js', array( 'rentfetch-flatpickr-script' ), RENTFETCH_VERSION, true );
-    
+
     // Properties map search
     wp_register_style( 'rentfetch-search-properties-map', RENTFETCH_PATH . 'css/search-properties-map.css', array(), RENTFETCH_VERSION, 'screen' );
     wp_register_script( 'rentfetch-search-properties-ajax', RENTFETCH_PATH . 'js/rentfetch-search-properties-ajax.js', array( 'jquery' ), RENTFETCH_VERSION, true );
     wp_register_script( 'rentfetch-search-properties-script', RENTFETCH_PATH . 'js/rentfetch-search-properties-script.js', array( 'jquery' ), RENTFETCH_VERSION, true );
     wp_register_script( 'rentfetch-toggle-map', RENTFETCH_PATH . 'js/rentfetch-toggle-map.js', array( 'jquery' ), RENTFETCH_VERSION, true );
-    
+
     // Properties map (the map itself)
     wp_register_script( 'rentfetch-property-map', RENTFETCH_PATH . 'js/rentfetch-property-map.js', array( 'jquery', 'rentfetch-google-maps' ), RENTFETCH_VERSION, true );
     wp_register_script( 'rentfetch-single-property-map', RENTFETCH_PATH . 'js/rentfetch-single-property-map.js', array( 'jquery', 'rentfetch-google-maps' ), RENTFETCH_VERSION, true );
-    
+
     // Properties searchbar
     wp_register_script( 'rentfetch-search-filters-general', RENTFETCH_PATH . 'js/rentfetch-search-filters-general.js', array( 'jquery' ), RENTFETCH_VERSION, true );
-        
+
     // Properties in archive
     wp_register_style( 'rentfetch-properties-in-archive', RENTFETCH_PATH . 'css/properties-in-archive.css', array(), RENTFETCH_VERSION, 'screen' );
     wp_register_script( 'rentfetch-property-images-slider-init', RENTFETCH_PATH . 'js/rentfetch-property-images-slider-init.js', array( 'jquery' ), RENTFETCH_VERSION, true );
-    
+
     // Favorite properties
     wp_register_script( 'rentfetch-property-favorites-cookies', 'https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js', array( 'jquery' ), RENTFETCH_VERSION, true );
     wp_register_script( 'rentfetch-property-favorites', RENTFETCH_PATH . 'js/rentfetch-property-favorites.js', array( 'rentfetch-property-favorites-cookies' ), RENTFETCH_VERSION, true );
-    
+
     // Floorplans in archive
     wp_register_style( 'rentfetch-floorplan-in-archive', RENTFETCH_PATH . 'css/floorplan-in-archive.css', array(), RENTFETCH_VERSION, 'screen' );
     wp_register_script( 'rentfetch-floorplan-images-slider-init', RENTFETCH_PATH . 'js/rentfetch-floorplan-images-slider-init.js', array( 'jquery' ), RENTFETCH_VERSION, true );
-    
+
     // Fancybox
     wp_register_style( 'rentfetch-fancybox-style', RENTFETCH_PATH . 'vendor/fancybox/jquery.fancybox.min.css', array(), RENTFETCH_VERSION, 'screen' );
     wp_register_script( 'rentfetch-fancybox-script', RENTFETCH_PATH . 'vendor/fancybox/jquery.fancybox.min.js', array( 'jquery' ), RENTFETCH_VERSION, true );
-        
+
     // Slick
     wp_register_script( 'rentfetch-slick-main-script', RENTFETCH_PATH . 'vendor/slick/slick.min.js', array('jquery'), RENTFETCH_VERSION, true );
     wp_register_style( 'rentfetch-slick-main-styles', RENTFETCH_PATH . 'vendor/slick/slick.css', array(), RENTFETCH_VERSION );
     wp_register_style( 'rentfetch-slick-main-theme', RENTFETCH_PATH . 'vendor/slick/slick-theme.css', array(), RENTFETCH_VERSION );
-    
+
     // Google reCAPTCHA
     wp_register_script( 'rentfetch-google-recaptcha', 'https://www.google.com/recaptcha/api.js', array('jquery'), RENTFETCH_VERSION, true );
-    
-    	
+
+
 }
 
 ////////////////////
@@ -361,32 +361,52 @@ function console_log( $data ){
     echo '</script>';
 }
 
+/**
+ * Should we enable our [verbose] log file(s)?
+ *
+ * Dependencies:
+ *   RENT_FETCH_LOG can be enabled by defining it 'true' in the wp-config.php file.
+ */
+function rf_is_enable_log() {
+    if ( ! defined( 'RENT_FETCH_LOG' ) ) {
+        define( 'RENT_FETCH_LOG', false );
+    };
+
+    return RENT_FETCH_LOG;
+}
+
 //* Add debug logging
-function rentfetch_log($message) { 
-    
+function rentfetch_log($message) {
+    if ( ! rf_is_enable_log() ) {
+        return;
+    }
+
     if( is_array( $message ) )
-        $message = json_encode($message); 
-        
+        $message = json_encode($message);
+
     $file = fopen( WP_CONTENT_DIR . "/apartment-sync-debug.log", "a" );
-    fwrite($file, date('Y-m-d h:i:s') . " " . $message . "\n"); 
-    fclose($file); 
-    
+    fwrite($file, date('Y-m-d h:i:s') . " " . $message . "\n");
+    fclose($file);
+
     $file = fopen( WP_CONTENT_DIR . "/apartment-sync-debug-verbose.log", "a" );
-    fwrite($file, date('Y-m-d h:i:s') . " " . $message . "\n"); 
-    fclose($file); 
-    
+    fwrite($file, date('Y-m-d h:i:s') . " " . $message . "\n");
+    fclose($file);
+
 }
 
 //* Add debug verbose logging
-function rentfetch_verbose_log($message) { 
-    
+function rentfetch_verbose_log($message) {
+    if ( ! rf_is_enable_log() ) {
+        return;
+    }
+
     if( is_array( $message ) )
-        $message = json_encode($message); 
-                
+        $message = json_encode($message);
+
     $file = fopen( WP_CONTENT_DIR . "/apartment-sync-debug-verbose.log", "a" );
-    fwrite($file, date('Y-m-d h:i:s') . " " . $message . "\n"); 
-    fclose($file); 
-    
+    fwrite($file, date('Y-m-d h:i:s') . " " . $message . "\n");
+    fclose($file);
+
 }
 
 //////////////////////////////
